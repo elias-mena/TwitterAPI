b375ea2aa3889a95face2eec204ba4480419453f
Elias Mena <eliasmena.2911@gmail.com>
2021-12-23 22:07:41 -0600

    Initial configuration

---
+++ main.py
@@ -0,0 +1,11 @@
+import uvicorn # Servidor
+from fastapi import FastAPI
+
+app = FastAPI()
+
+app.get(path="/")
+def home():
+    return {"Twitter API":"Working!"}
+
+if __name__ == "__main__":
+    uvicorn.run('main:app', host="127.0.0.1", port=8000, reload=True)
