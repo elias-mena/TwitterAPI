68dafd5b8d93c0f457292c7bf891fe6c8130b696
Elias Mena <69024424+elias-mena@users.noreply.github.com>
2022-04-30 23:30:30 -0600

    Update schemas.py
    Some comments

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -1,5 +1,9 @@
 def serializeDict(a) -> dict:
+    """ Serialize BSON to dict
+    """
     return {**{i:str(a[i]) for i in a if i=='_id'},**{i:a[i] for i in a if i!='_id'}}
 
 def serializeList(entity) -> list:
+    """ Serialize List[BSON] to List[Dict]
+    """
     return [serializeDict(a) for a in entity]
